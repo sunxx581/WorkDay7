bdc0a9e2f23eb51c9205590a4a615aa1cd769f05
idsc3101 <idsc3101@gmail.com>
2014-10-08 17:12:12 -0500

    in class

--- Millionaire/Millionaire/Form1.vb
+++ Millionaire/Millionaire/Form1.vb
@@ -13,11 +13,17 @@ Public Class Form1
         ' Convert into a number 
         Dim balance As Double, numYears As Integer
         balance = CDbl(amountSt)
-
+        Dim r As Double = 0.06
         ' Compute the time it takes to reach the balance of a million
         Do While balance < 1000000
             balance += 0.06 * balance
             numYears += 1
+            If numYears <= 10 Then
+                r += 0.005
+            End If
+            If numYears > 10 And r > 0.01 Then
+                r = 0.005
+            End If
         Loop
 
         txtWhen.Text = "In " & numYears & " years you will have a million dollars."
